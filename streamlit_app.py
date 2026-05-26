900104784b0fd4d079a6da0ecb7f4d1f5f552612
nailasyairi-lgtm <nailasyairi@gmail.com>
2026-05-26 20:38:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
-import streamlit as st
+from flask import Flask, render_template
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+app = Flask(__name__)
+
+# Home
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Halaman simulasi menimbang
+@app.route('/timbang')
+def timbang():
+    return render_template('timbang.html')
+
+# Halaman simulasi titrasi
+@app.route('/titrasi')
+def titrasi():
+    return render_template('titrasi.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
